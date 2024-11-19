bc0b91fe956c2c553c54d9ab6693efb7622b6d28
NicolasCBV <nicolascbv700@gmail.com>
2024-11-19 15:06:24 -0300

    fix(buildFirebaseConfig.sh): prevent input file duplication

--- cmd/buildFirebaseConfig.sh
+++ cmd/buildFirebaseConfig.sh
@@ -1,5 +1,10 @@
 #!/bin/bash
 
+if [ -f ./.firebaserc ]; then
+	echo ".firebaserc already exist" 
+	exit 0
+fi
+
 if [ -z "${FIREBASE_PROJECT}" ]; then
 	echo "Could not find FIREBASE_PROJECT variable"
 	exit 1
